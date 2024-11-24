a3181aeee5ae779b40d1a1473f9b63bec6b678ae
Vlad Aleksandrov <hilk.2013@yandex.ru>
2024-11-24 15:49:31 +0300

    [fix] del useless modules

--- webpack.config.js
+++ webpack.config.js
@@ -2,8 +2,6 @@
 
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const HtmlMinimizerPlugin = require("html-minimizer-webpack-plugin");
-const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const TerserPlugin = require("terser-webpack-plugin");
 const CopyPlugin = require("copy-webpack-plugin");
