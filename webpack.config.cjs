9ba0925d6b6947dc30535667f95f3fc41ba7403d
Vlad Aleksandrov <hilk.2013@yandex.ru>
2025-01-26 15:49:44 +0300

    Revert "[feat] add vite"

---
+++ webpack.config.cjs
@@ -0,0 +1,90 @@
+'use strict'
+
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const CopyPlugin = require('copy-webpack-plugin')
+const webpack = require('webpack')
+const buildPath = path.resolve(__dirname, 'build')
+
+const htmlPageNames = ['credits', 'download', 'features', 'updates']
+const multipleHtmlPlugins = htmlPageNames.map((name) => {
+  return new HtmlWebpackPlugin({
+    template: `./src/${name}.html`,
+    filename: `${name}.html`,
+    inject: true,
+    chunks: [`${name}`]
+  })
+})
+const handler = (percentage, message, ...args) => {
+  console.info(percentage, message, ...args)
+}
+
+module.exports = {
+  mode: 'production',
+  devServer: {
+    static: {
+      directory: buildPath
+    },
+    port: 3000,
+    open: true,
+    compress: true
+  },
+
+  performance: {
+    hints: false,
+    maxEntrypointSize: 512000,
+    maxAssetSize: 512000
+  },
+
+  entry: './src/js/script.js',
+  stats: 'errors-only',
+
+  output: {
+    filename: 'script.js',
+    path: buildPath,
+    clean: true,
+    assetModuleFilename: 'img/[name][ext]'
+  },
+
+  plugins: [
+    new webpack.ProgressPlugin(handler),
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      favicon: './src/img/favicon.ico',
+      inject: true,
+      chunks: ['index']
+    }),
+    new MiniCssExtractPlugin(),
+    new CopyPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, 'src/img/'),
+          to: path.resolve(__dirname, 'build/img/')
+        }
+      ]
+    })
+  ].concat(multipleHtmlPlugins),
+
+  module: {
+    rules: [
+      {
+        test: /\.(png|webp|jpe?g|gif|svg)$/i,
+        exclude: /node_modules/,
+        type: 'asset/resource'
+      },
+      {
+        test: /\.(woff(2)?|eot|ttf|otf)$/i,
+        exclude: /node_modules/,
+        generator: {
+          filename: 'font/[name][ext]'
+        }
+      },
+      {
+        test: /\.(s?css)$/i,
+        exclude: /node_modules/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
+      }
+    ]
+  }
+}
